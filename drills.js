401cf1a5b8056d829a527a94d189d9d40a7a14bc
shimmy77 <christianshim@gmail.com>
2020-01-06 14:57:44 -0600

    added value

--- drills.js
+++ drills.js
@@ -1,8 +1,10 @@
 function createGreeting(name, age){
-    return('Hi, my name is Chris and I ' m 29 years old);
+    return('Hi, my name is Chris and I ' m 29 years old' + 'I was born in [yearOfBirth]);
+    let yearOfBirth = (2020 - age);
 }
 
 createGreeting();
 
 const greeting1 = createGreeting();
 console.log(greeting1)
+
