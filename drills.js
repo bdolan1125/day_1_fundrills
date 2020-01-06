b3331913a34d75df28354e2522019ef4a34751c2
shimmy77 <christianshim@gmail.com>
2020-01-06 15:01:34 -0600

    added yob and greeting functions

--- drills.js
+++ drills.js
@@ -3,6 +3,14 @@ function createGreeting(name, age){
     let yearOfBirth = (2020 - age);
 }
 
+function getYearOfBirth(age) {
+    return (2020 - age);
+}
+function createGreeting(name, age) {
+    const yob = getYearOfBirth;
+    return('Hi, my name is Chris and I ' m 29 years old' + 'I was born in [yearOfBirth]);
+}
+
 createGreeting();
 
 const greeting1 = createGreeting();
