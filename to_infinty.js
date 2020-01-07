8f7a20402a38edd7ca152459d09accf7915b0f91
Benjamin Dolan <benjamin@thinkful.com>
2020-01-06 17:53:58 -0700

    finished jedi.js and moved code to to_infinity.js

---
+++ to_infinty.js
@@ -0,0 +1,14 @@
+function beyond(num){
+    if(num === Infinity){
+        console.log('And Beyond')
+    }
+    if(num >0){
+        console.log("To Infinity")
+    }
+    if(num <0){
+        console.log("To Negative Infinity")
+    }
+    if(num === 0){
+        console.log("Staying Home")
+    }
+}
